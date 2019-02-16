dfd7149973c67997b1a540b1157a08c75463b5df
mojtabakaviani <mojtaba.kaviani@gmail.com>
2019-02-16 19:22:15 +0330

    bump to version 4.3.1

---
+++ package.js
@@ -0,0 +1,15 @@
+Package.describe({
+    name: 'mojtabakaviani:bootstrap.rtl',
+    summary: 'The most popular front-end framework for developing responsive, mobile first projects on the web.',
+    version: '4.3.1',
+    git: 'https://github.com/mojtabakaviani/bootstrap-rtl.git'
+});
+
+Package.onUse(function (api) {
+    api.versionsFrom('METEOR@1.0');
+    api.use('jquery', 'client');
+    api.addFiles([
+        'dist/css/bootstrap.css',
+        'dist/js/bootstrap.js'
+    ], 'client');
+});
